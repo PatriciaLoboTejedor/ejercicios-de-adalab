eacccb5500a5282372d5e8b82ed78760a2bbb875
PatriciaLoboTejedor <patricialobotejedor@gmail.com>
2021-05-04 11:11:07 +0200

    change exercicie 9

--- modulo-2/modulo-2-leccion-02-intro-a-la-programación-2/modulo-2-leccion-02-ejercicio-09-contar-letras-INCOM/main.js
+++ modulo-2/modulo-2-leccion-02-intro-a-la-programación-2/modulo-2-leccion-02-ejercicio-09-contar-letras-INCOM/main.js
@@ -1,13 +1,29 @@
 "user strict";
 
+//OPTION1 - ESCRIBIR Y REASIGNAR CLASES CON CONSTANTES
 const text = document.querySelector("body");
-text.innerHTML +=
-  "<p class='js-text'>El nombre de mi compañera es <span class='js-surname'>Leticia Fernández Sánchez</span>, y está compuesto por <span class='js-number'> 23 </span>caracteres</p>";
+text.innerHTML =
+  "<p class='js-text'>El nombre de mi compañera es <span class='js-name'>Leticia Fernández Sánchez</span>, y está compuesto por <span class='js-number'> 23 </span> caracteres</p>";
 
-const surname = document.querySelector(".js-surname");
-surname.innerHTML = "Rosa Berned";
+const name = document.querySelector(".js-name");
+name.innerHTML = "Rosa Berned";
 
 const number = document.querySelector(".js-number");
-number.innerHTML = surname.lenght;
+number.innerHTML = name.innerHTML.length;
 
-console.log(number);
+console.log(name);
+
+//OPTION2 - CONSTANTES + ESCRIBIR CONCATENANDO CONSTANTES
+/*const name = "Rosa Berned";
+const number = name.length;
+
+const text = document.querySelector("body");
+
+text.innerHTML =
+  "El nombre de mi compañera es " +
+  name +
+  ", y está compuesto por " +
+  number +
+  " caracteres";
+
+console.log(number);*/
